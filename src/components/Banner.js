f4aeb12987166b02021b983d25344efb484c2341
5AMA50N <adityakamble.quicklabs1@gmail.com>
2025-01-31 23:15:02 +0530

    testing ssh

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import resume from './Resume.pdf';
 
-
+//testing pass ke required or not
 function Banner() {
     return(
         <section className="banner" id="banner">
